1605c4383849f216d7ab27ac04bd728e0417b2d8
Pylgos <43234674+Pylgos@users.noreply.github.com>
2025-01-06 11:10:18 +0900

    ransac_localization: Change default name

--- src/ransac_localization/src/standalone.cpp
+++ src/ransac_localization/src/standalone.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   std::shared_ptr<rclcpp::Node> node =
       std::make_shared<ransac_localization::RansacLocalizationNode>(
-          "ransac_localization_node");
+          "ransac_localization");
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
